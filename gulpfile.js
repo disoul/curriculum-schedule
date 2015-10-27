799523441009582e8e96b9660c68000d4e551334
disoul <starky_ds@sina.cn>
2015-10-27 15:53:51 +0800

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    compass = require('gulp-compass'),
+	path = require('path');
+
+gulp.task('compass',function(){
+	gulp.src('./public/sass/index.scss')
+	.pipe(compass({
+		project: path.join(__dirname,'./public'),
+		css: 'css',
+		sass: 'sass',
+		image: 'image'
+	}));
+});
+
+gulp.task('sass:watch',function(){
+	gulp.watch('./public/scss/**/*.sass',['compass']);
+});
+
+gulp.task('default', function() {
+	['sass:watch']
+});
+
