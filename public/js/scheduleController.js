536f74b689bd70304ca146b0ca9c4cf5c47214bd
disoul <starky_ds@sina.cn>
2015-10-28 16:37:25 +0800

    实现布局算法

--- public/js/scheduleController.js
+++ public/js/scheduleController.js
@@ -2,5 +2,26 @@ app.controller('scheduleController', ['$scope',
     function($scope) {
         $scope.days = ['一', '二', '三', '四', '五', '六', '日'];
         $scope.index = range(12, 1); 
-    }]
+        $scope.color = function(num) {
+            if (num == 1) return '#F8F8F8';
+            return COLORS[Math.floor(Math.random() * COLORS.length)];
+        };
+        $scope.getTile = (function() {
+            var array = [];
+            for (var i=0;i<12;i++) {
+                for (var j=0;j<7;j++) {
+                    if (test[i][j] === 0) {
+                        array.push(1);
+                    }else if((i>0) && (test[i-1][j] < test[i][j])) {
+                        array.push(test[i][j]);
+                    }
+                    if ((i ===0) && (test[i][j] !== 0)) {
+                        array.push(test[i][j]);
+                    }
+                }
+            }
+            console.log(array);
+            return array;
+        })();
+    }] 
 );
